4b5560da605f6bbe1b4f6adeabbe3680825f3ab5
alunounifg4 <alunounifg4@gmail.com>
2023-12-04 22:47:16 -0300

    Correção dos testes

--- teste/test_controllers.py
+++ teste/test_controllers.py
@@ -20,6 +20,59 @@ class TestStudents:
         """Function."""
         return conexao_cientifica_controllers.ConexaoCientificaControllers
 
+    # def test_valid_register_student(self, co_cie_con):
+    #     """Function valid login."""
+    #     c_c_m_student = conexao_cientifica_models_students.ConexaoCientificaModelsStudents(
+    #         full_name='João Silva',
+    #         email='gmail@gmail.com',
+    #         ra=1352113070
+    #     )
+    #     result = co_cie_con.register_students(c_c_m_student)
+
+    #     assert result == 'Aluno cadastrado com sucesso!'
+
+    # def test_invalid_register_student(self, co_cie_con):
+    #     """Function valid login."""
+    #     c_c_m_student = conexao_cientifica_models_students.ConexaoCientificaModelsStudents(
+    #         full_name='João Silva',
+    #         email='gmailgmail.com',
+    #         ra=1352113070
+    #     )
+
+    #     result = co_cie_con.register_students(c_c_m_student)
+
+    #     assert result == 'Aluno cadastrado com sucesso!'
+
+    # def test_valid_login(self, co_cie_con):
+    #     """Function valid login."""
+    #     c_c_m_student = conexao_cientifica_models_students.ConexaoCientificaModelsStudents(
+    #         full_name='João Silva',
+    #         email='gmail@gmail.com',
+    #         ra=1352113070
+    #     )
+    #     co_cie_con.register_students(c_c_m_student)
+    #     c_c_c_s = co_cie_con.login_students(
+    #         user='gmail@gmail.com',
+    #         password=1352113070
+    #     )
+    #     assert c_c_c_s == c_c_m_student.email
+
+    # def test_invalid_login(self, co_cie_con):
+    #     """Function invalid login."""
+    #     c_c_m_student = conexao_cientifica_models_students.ConexaoCientificaModelsStudents(
+    #         full_name='João Silva',
+    #         email='gmail@gmail.com',
+    #         ra=1352113070
+    #     )
+
+    #     co_cie_con.register_students(c_c_m_student)
+
+    #     c_c_c_s = co_cie_con.login_students(
+    #         user='carlos@gmail.com',
+    #         password=1352113070
+    #     )
+    #     assert c_c_c_s == c_c_m_student.email
+    
     # def test_valid_register_article(self, co_cie_con):
     #     """Function valid article."""
     #     c_c_m_student = conexao_cientifica_models_students.ConexaoCientificaModelsStudents(
@@ -70,35 +123,6 @@ class TestStudents:
     #     assert 'Artigo cadastrado com sucesso!' == result
     
 
-    # def test_valid_login(self, co_cie_con):
-    #     """Function valid login."""
-    #     c_c_m_student = conexao_cientifica_models_students.ConexaoCientificaModelsStudents(
-    #         full_name='João Silva',
-    #         email='gmail@gmail.com',
-    #         ra=1352113070
-    #     )
-    #     co_cie_con.register_students(c_c_m_student)
-    #     c_c_c_s = co_cie_con.login_students(
-    #         user='gmail@gmail.com',
-    #         password=1352113070
-    #     )
-    #     assert c_c_c_s == c_c_m_student.email
-
-    # def test_invalid_login(self, co_cie_con):
-    #     """Function invalid login."""
-    #     c_c_m_student = conexao_cientifica_models_students.ConexaoCientificaModelsStudents(
-    #         full_name='João Silva',
-    #         email='gmail@gmail.com',
-    #         ra=1352113070
-    #     )
-
-    #     co_cie_con.register_students(c_c_m_student)
-
-    #     c_c_c_s = co_cie_con.login_students(
-    #         user='carlos@gmail.com',
-    #         password=1352113070
-    #     )
-    #     assert c_c_c_s == c_c_m_student.email
 
 
    
